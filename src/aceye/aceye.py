b22d1850fe4d15dc1c1dd9e90bdae8a24e509aeb
John Capobianco <ptcapo@gmail.com>
2022-11-18 11:09:13 -0500

    compute domains

--- src/aceye/aceye.py
+++ src/aceye/aceye.py
@@ -44,7 +44,7 @@ class ACEye():
         parsed_json = json.dumps(self.fabric_nodes(), indent=4, sort_keys=True)
         self.all_files(parsed_json)
         parsed_json = json.dumps(self.physical_interfaces(), indent=4, sort_keys=True)
-        self.all_files(parsed_json)        
+        self.all_files(parsed_json)
         parsed_json = json.dumps(self.leaf_interface_profiles(), indent=4, sort_keys=True)
         self.all_files(parsed_json)
         parsed_json = json.dumps(self.spine_interface_profiles(), indent=4, sort_keys=True)
@@ -123,19 +123,23 @@ class ACEye():
         self.all_files(parsed_json)
         parsed_json = json.dumps(self.compute_controllers(), indent=4, sort_keys=True)
         self.all_files(parsed_json)
+        parsed_json = json.dumps(self.compute_domains(), indent=4, sort_keys=True)
+        self.all_files(parsed_json)
 
     def make_directories(self):
         api_list = ['Access Control Entities',
                     'Access Control Instances',
                     'Access Control Rules',
-                    'Cluster Aggregate Interfaces',
                     'Application Profiles',
                     'Attachable Access Entity Profiles',
                     'Audit Log',
                     'BGP Route Reflectors',
                     'Bridge Domains',
+                    'Cluster Aggregate Interfaces',
                     'Cluster Health',
+                    'Cluster Physical Interfaces',
                     'Compute Controllers',
+                    'Compute Domains',
                     'Contexts',
                     'Contracts',
                     'Contract Subjects',
@@ -598,6 +602,13 @@ class ACEye():
         response_dict  = response.json()
         return(response_dict)
 
+    def compute_domains(self):
+        self.url = f"{ self.aci }/api/node/class/compDom.json"
+        response = requests.request("GET", self.url, cookies = self.cookie, verify=False)
+        print(f"<Compute Domains code { response.status_code } for { self.url }>")
+        response_dict  = response.json()
+        return(response_dict)
+
     def json_file(self, parsed_json):
         if "Tenant" in self.url:
             with open('Tenant/JSON/Tenants.json', 'w' ) as f:
@@ -802,6 +813,10 @@ class ACEye():
 
         if "compCtrlr" in self.url:
             with open('Compute Controllers/JSON/Compute Controllers.json', 'w' ) as f:
+                f.write(parsed_json)
+
+        if "compDom" in self.url:
+            with open('Compute Domains/JSON/Compute Domains.json', 'w' ) as f:
                 f.write(parsed_json)
 
     def yaml_file(self, parsed_json):
@@ -1009,6 +1024,10 @@ class ACEye():
 
         if "compCtrlr" in self.url:
             with open('Compute Controllers/YAML/Compute Controllers.yaml', 'w' ) as f:
+                f.write(clean_yaml)
+
+        if "compDom" in self.url:
+            with open('Compute Domains/YAML/Compute Domains.yaml', 'w' ) as f:
                 f.write(clean_yaml)
 
     def csv_file(self, parsed_json):
@@ -1220,6 +1239,10 @@ class ACEye():
 
         if "compCtrlr" in self.url:
             with open('Compute Controllers/CSV/Compute Controllers.csv', 'w' ) as f:
+                f.write(csv_output)
+
+        if "compDom" in self.url:
+            with open('Compute Domains/CSV/Compute Domains.csv', 'w' ) as f:
                 f.write(csv_output)
 
     def markdown_file(self, parsed_json):
@@ -1434,6 +1457,10 @@ class ACEye():
             with open('Compute Controllers/Markdown/Compute Controllers.md', 'w' ) as f:
                 f.write(markdown_output)
 
+        if "compDom" in self.url:
+            with open('Compute Domains/Markdown/Compute Domains.md', 'w' ) as f:
+                f.write(markdown_output)
+
     def html_file(self, parsed_json):
         template_dir = Path(__file__).resolve().parent
         env = Environment(loader=FileSystemLoader(str(template_dir)))
@@ -1646,6 +1673,10 @@ class ACEye():
             with open('Compute Controllers/HTML/Compute Controllers.html', 'w' ) as f:
                 f.write(html_output)
 
+        if "compDom" in self.url:
+            with open('Compute Domains/HTML/Compute Domains.html', 'w' ) as f:
+                f.write(html_output)
+
     def mindmap_file(self, parsed_json):
         template_dir = Path(__file__).resolve().parent
         env = Environment(loader=FileSystemLoader(str(template_dir)))
@@ -1856,6 +1887,10 @@ class ACEye():
 
         if "compCtrlr" in self.url:
             with open('Compute Controllers/Mindmap/Compute Controllers.md', 'w' ) as f:
+                f.write(mindmap_output)
+
+        if "compDom" in self.url:
+            with open('Compute Domains/Mindmap/Compute Domains.md', 'w' ) as f:
                 f.write(mindmap_output)
 
     def all_files(self, parsed_json):
