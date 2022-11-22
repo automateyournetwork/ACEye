79e967c6f650872eb276e3243f5ca3b3ecf51450
John Capobianco <johcapob@cisco.com>
2022-11-22 14:36:49 -0500

    bgp domain af

--- src/aceye/aceye.py
+++ src/aceye/aceye.py
@@ -145,6 +145,8 @@ class ACEye():
         self.all_files(parsed_json)
         parsed_json = json.dumps(self.bgp_domains(), indent=4, sort_keys=True)
         self.all_files(parsed_json)
+        parsed_json = json.dumps(self.bgp_domain_af(), indent=4, sort_keys=True)
+        self.all_files(parsed_json)
 
     def make_directories(self):
         api_list = ['Access Control Entities',
@@ -160,6 +162,7 @@ class ACEye():
                     'ARP Interfaces',
                     'Attachable Access Entity Profiles',
                     'Audit Log',
+                    'BGP Domain Address Families',
                     'BGP Domains',
                     'BGP Route Reflectors',
                     'Bridge Domains',
@@ -709,6 +712,13 @@ class ACEye():
         response_dict  = response.json()
         return(response_dict)
 
+    def bgp_domain_af(self):
+        self.url = f"{ self.aci }/api/node/class/bgpDomAf.json"
+        response = requests.request("GET", self.url, cookies = self.cookie, verify=False)
+        print(f"<BGP Domain Address Family code { response.status_code } for { self.url }>")
+        response_dict  = response.json()
+        return(response_dict)        
+
     def json_file(self, parsed_json):
         if "Tenant" in self.url:
             with open('Tenant/JSON/Tenants.json', 'w' ) as f:
@@ -956,8 +966,12 @@ class ACEye():
                 f.write(parsed_json)
 
         if "bgpDom" in self.url:
-            with open('BGP Domains/JSON/BGP Domains.json', 'w' ) as f:
-                f.write(parsed_json)
+            if "Af" in self.url:   
+                with open('BGP Domain Address Families/JSON/BGP Domain Address Families.json', 'w' ) as f:
+                    f.write(parsed_json)
+            else:
+                with open('BGP Domains/JSON/BGP Domains.json', 'w' ) as f:
+                    f.write(parsed_json)
 
     def yaml_file(self, parsed_json):
         clean_yaml = yaml.dump(json.loads(parsed_json), default_flow_style=False)
@@ -1207,8 +1221,12 @@ class ACEye():
                 f.write(clean_yaml)
 
         if "bgpDom" in self.url:
-            with open('BGP Domains/YAML/BGP Domains.yaml', 'w' ) as f:
-                f.write(clean_yaml)
+            if "Af" in self.url:
+                with open('BGP Domain Address Families/YAML/BGP Domain Address Families.yaml', 'w' ) as f:
+                    f.write(clean_yaml)
+            else:
+                with open('BGP Domains/YAML/BGP Domains.yaml', 'w' ) as f:
+                    f.write(clean_yaml)
 
     def csv_file(self, parsed_json):
         template_dir = Path(__file__).resolve().parent
@@ -1462,8 +1480,12 @@ class ACEye():
                 f.write(csv_output)
 
         if "bgpDom" in self.url:
-            with open('BGP Domains/CSV/BGP Domains.csv', 'w' ) as f:
-                f.write(csv_output)
+            if "Af" in self.url:
+                with open('BGP Domain Address Families/CSV/BGP Domain Address Families.csv', 'w' ) as f:
+                    f.write(csv_output)
+            else:
+                with open('BGP Domains/CSV/BGP Domains.csv', 'w' ) as f:
+                    f.write(csv_output)
 
     def markdown_file(self, parsed_json):
         template_dir = Path(__file__).resolve().parent
@@ -1718,8 +1740,12 @@ class ACEye():
                 f.write(markdown_output)
 
         if "bgpDom" in self.url:
-            with open('BGP Domains/Markdown/BGP Domains.md', 'w' ) as f:
-                f.write(markdown_output)
+            if "Af" in self.url:
+                with open('BGP Domain Address Families/Markdown/BGP Domain Address Families.md', 'w' ) as f:
+                    f.write(markdown_output)
+            else:
+                with open('BGP Domains/Markdown/BGP Domains.md', 'w' ) as f:
+                    f.write(markdown_output)
 
     def html_file(self, parsed_json):
         template_dir = Path(__file__).resolve().parent
@@ -1974,8 +2000,12 @@ class ACEye():
                 f.write(html_output)
 
         if "bgpDom" in self.url:
-            with open('BGP Domains/HTML/BGP Domains.html', 'w' ) as f:
-                f.write(html_output)
+            if "Af" in self.url:
+                with open('BGP Domain Address Families/HTML/BGP Domain Address Families.html', 'w' ) as f:
+                    f.write(html_output)
+            else:
+                with open('BGP Domains/HTML/BGP Domains.html', 'w' ) as f:
+                    f.write(html_output)
 
     def mindmap_file(self, parsed_json):
         template_dir = Path(__file__).resolve().parent
@@ -2230,8 +2260,12 @@ class ACEye():
                 f.write(mindmap_output)
 
         if "bgpDom" in self.url:
-            with open('BGP Domains/Mindmap/BGP Domains.md', 'w' ) as f:
-                f.write(mindmap_output)
+            if "bgpDomAf" in self.url:
+                with open('BGP Domain Address Families/Mindmap/BGP Domain Address Families.md', 'w' ) as f:
+                    f.write(mindmap_output)
+            else:
+                with open('BGP Domains/Mindmap/BGP Domains.md', 'w' ) as f:
+                    f.write(mindmap_output)
 
     def all_files(self, parsed_json):
         self.json_file(parsed_json)
