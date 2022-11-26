2ee0303944481ee3fbe1acbf4ce28c0f8bf154cc
John Capobianco <ptcapo@gmail.com>
2022-11-26 15:54:41 -0500

    leaf ports

--- README.md
+++ README.md
@@ -120,6 +120,8 @@ Equipment Field Programmable Gate Arrays
 
 Equipment Indicator LEDs
 
+Equipment Leaf Ports
+
 Equipment Line Card Slots
 
 Equipment Line Cards
--- src/aceye/aceye.py
+++ src/aceye/aceye.py
@@ -203,6 +203,8 @@ class ACEye():
         self.all_files(parsed_json)
         parsed_json = json.dumps(self.equipment_line_card_slots(), indent=4, sort_keys=True)
         self.all_files(parsed_json)
+        parsed_json = json.dumps(self.equipment_leaf_ports(), indent=4, sort_keys=True)
+        self.all_files(parsed_json)
 
     def make_directories(self):
         api_list = ['Access Control Entities',
@@ -262,6 +264,7 @@ class ACEye():
                     'Equipment Fans',
                     'Equipment Field Programmable Gate Arrays',
                     'Equipment Indicator LEDs',
+                    'Equipment Leaf Ports',
                     'Equipment Line Card Slots',
                     'Equipment Line Cards',
                     'Events',
@@ -1007,6 +1010,13 @@ class ACEye():
         response_dict  = response.json()
         return response_dict
 
+    def equipment_leaf_ports(self):
+        self.url = f"{ self.aci }/api/node/class/eqptLeafP.json"
+        response = requests.request("GET", self.url, cookies = self.cookie, verify=False)
+        print(f"<Equipment Leaf Ports code { response.status_code } for { self.url }>")
+        response_dict  = response.json()
+        return response_dict
+
     def json_file(self, parsed_json):
         if "Tenant" in self.url:
             with open('Tenant/JSON/Tenants.json', 'w' ) as f:
@@ -1375,6 +1385,10 @@ class ACEye():
             else:
                 with open('Equipment Line Cards/JSON/Equipment Line Cards.json', 'w' ) as f:
                     f.write(parsed_json)
+
+        if "eqptLeafP" in self.url:
+            with open('Equipment Leaf Ports/JSON/Equipment Leaf Ports.json', 'w' ) as f:
+                f.write(parsed_json)
 
     def yaml_file(self, parsed_json):
         clean_yaml = yaml.dump(json.loads(parsed_json), default_flow_style=False)
@@ -1745,6 +1759,10 @@ class ACEye():
             else:
                 with open('Equipment Line Cards/YAML/Equipment Line Cards.yaml', 'w' ) as f:
                     f.write(clean_yaml)
+
+        if "eqptLeafP" in self.url:
+            with open('Equipment Leaf Ports/YAML/Equipment Leaf Ports.yaml', 'w' ) as f:
+                f.write(clean_yaml)
 
     def csv_file(self, parsed_json):
         template_dir = Path(__file__).resolve().parent
@@ -2119,7 +2137,11 @@ class ACEye():
             else:
                 with open('Equipment Line Cards/CSV/Equipment Line Cards.csv', 'w' ) as f:
                     f.write(csv_output)
-                
+
+        if "eqptLeafP" in self.url:
+            with open('Equipment Leaf Ports/CSV/Equipment Leaf Ports.csv', 'w' ) as f:
+                f.write(csv_output)
+
     def markdown_file(self, parsed_json):
         template_dir = Path(__file__).resolve().parent
         env = Environment(loader=FileSystemLoader(str(template_dir)))
@@ -2494,6 +2516,10 @@ class ACEye():
             else:
                 with open('Equipment Line Cards/Markdown/Equipment Line Cards.md', 'w' ) as f:
                     f.write(markdown_output)
+
+        if "eqptLeafP" in self.url:
+            with open('Equipment Leaf Ports/Markdown/Equipment Leaf Ports.md', 'w' ) as f:
+                f.write(markdown_output)
 
     def html_file(self, parsed_json):
         template_dir = Path(__file__).resolve().parent
@@ -2870,6 +2896,10 @@ class ACEye():
                 with open('Equipment Line Cards/HTML/Equipment Line Cards.html', 'w' ) as f:
                     f.write(html_output)
 
+        if "eqptLeafP" in self.url:
+            with open('Equipment Leaf Ports/HTML/Equipment Leaf Ports.html', 'w' ) as f:
+                f.write(html_output)
+
     def mindmap_file(self, parsed_json):
         template_dir = Path(__file__).resolve().parent
         env = Environment(loader=FileSystemLoader(str(template_dir)))
@@ -3239,6 +3269,10 @@ class ACEye():
 
         if "eqptLC" in self.url:
             with open('Equipment Line Cards/Mindmap/Equipment Line Cards.md', 'w' ) as f:
+                f.write(mindmap_output)
+
+        if "eqptLeafP" in self.url:
+            with open('Equipment Leaf Ports/Mindmap/Equipment Leaf Ports.md', 'w' ) as f:
                 f.write(mindmap_output)
 
     def all_files(self, parsed_json):
