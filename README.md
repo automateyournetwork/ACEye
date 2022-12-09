9c2c3a8110aa9f68f22276d919f0b1fb44551a39
John Capobianco <ptcapo@gmail.com>
2022-12-09 08:46:41 -0500

    ospf instances

--- README.md
+++ README.md
@@ -376,6 +376,8 @@ OSPF Entities
 
 OSPF External Profiles
 
+OSPF Instances
+
 OSPF Interfaces
 
 Path Attachments
--- src/aceye/aceye.py
+++ src/aceye/aceye.py
@@ -454,6 +454,8 @@ class ACEye():
         self.all_files(parsed_json)
         parsed_json = json.dumps(self.ospf_interfaces(), indent=4, sort_keys=True)
         self.all_files(parsed_json)
+        parsed_json = json.dumps(self.ospf_instances(), indent=4, sort_keys=True)
+        self.all_files(parsed_json)
 
     def make_directories(self):
         api_list = ['Access Bundle Groups',
@@ -641,6 +643,7 @@ class ACEye():
                     'OSPF Domains',
                     'OSPF Entities',
                     'OSPF External Profiles',
+                    'OSPF Instances',
                     'OSPF Interfaces',
                     'Path Attachments',
                     'Physical Domains',
@@ -2210,6 +2213,13 @@ class ACEye():
         response_dict  = response.json()
         return response_dict
 
+    def ospf_instances(self):
+        self.url = f"{ self.aci }/api/node/class/ospfInst.json"
+        response = requests.request("GET", self.url, cookies = self.cookie, verify=False)
+        print(f"<OSPF Instances code { response.status_code } for { self.url }>")
+        response_dict  = response.json()
+        return response_dict
+
     def json_file(self, parsed_json):
         if "Tenant" in self.url:
             with open('Tenant/JSON/Tenants.json', 'w' ) as f:
@@ -3049,6 +3059,10 @@ class ACEye():
 
         if "ospfIf" in self.url:
             with open('OSPF Interfaces/JSON/OSPF Interfaces.json', 'w' ) as f:
+                f.write(parsed_json)
+
+        if "ospfInst" in self.url:
+            with open('OSPF Instances/JSON/OSPF Instances.json', 'w' ) as f:
                 f.write(parsed_json)
 
     def yaml_file(self, parsed_json):
@@ -3895,6 +3909,10 @@ class ACEye():
 
         if "ospfIf" in self.url:
             with open('OSPF Interfaces/YAML/OSPF Interfaces.yaml', 'w' ) as f:
+                f.write(clean_yaml)
+
+        if "ospfInst" in self.url:
+            with open('OSPF Instances/YAML/OSPF Instances.yaml', 'w' ) as f:
                 f.write(clean_yaml)
 
     def csv_file(self, parsed_json):
@@ -4745,6 +4763,10 @@ class ACEye():
 
         if "ospfIf" in self.url:
             with open('OSPF Interfaces/CSV/OSPF Interfaces.csv', 'w' ) as f:
+                f.write(csv_output)
+
+        if "ospfInst" in self.url:
+            with open('OSPF Instances/CSV/OSPF Instances.csv', 'w' ) as f:
                 f.write(csv_output)
 
     def markdown_file(self, parsed_json):
@@ -5598,6 +5620,10 @@ class ACEye():
             with open('OSPF Interfaces/Markdown/OSPF Interfaces.md', 'w' ) as f:
                 f.write(markdown_output)
 
+        if "ospfInst" in self.url:
+            with open('OSPF Instances/Markdown/OSPF Instances.md', 'w' ) as f:
+                f.write(markdown_output)
+
     def html_file(self, parsed_json):
         template_dir = Path(__file__).resolve().parent
         env = Environment(loader=FileSystemLoader(str(template_dir)))
@@ -6449,6 +6475,10 @@ class ACEye():
             with open('OSPF Interfaces/HTML/OSPF Interfaces.html', 'w' ) as f:
                 f.write(html_output)
 
+        if "ospfInst" in self.url:
+            with open('OSPF Instances/HTML/OSPF Instances.html', 'w' ) as f:
+                f.write(html_output)
+
     def mindmap_file(self, parsed_json):
         template_dir = Path(__file__).resolve().parent
         env = Environment(loader=FileSystemLoader(str(template_dir)))
@@ -7294,6 +7324,10 @@ class ACEye():
 
         if "ospfIf" in self.url:
             with open('OSPF Interfaces/Mindmap/OSPF Interfaces.md', 'w' ) as f:
+                f.write(mindmap_output)
+
+        if "ospfInst" in self.url:
+            with open('OSPF Instances/Mindmap/OSPF Instances.md', 'w' ) as f:
                 f.write(mindmap_output)
 
     def all_files(self, parsed_json):
